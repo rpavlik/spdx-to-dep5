43a5268451a110dfe6fd5b58d2ca1c0b09c8d13f
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-12 17:38:48 -0500

    wip

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0 OR MIT
  */
 
-use std::{collections::HashMap, convert::TryInto, ops::RangeBounds, pin::Pin};
+use std::{collections::HashMap, convert::TryInto, io::BufRead, ops::RangeBounds, pin::Pin};
 
 use async_std::{
     fs::{File, OpenOptions},
@@ -16,6 +16,8 @@ use futures::{io::Lines, prelude::*, AsyncBufReadExt};
 use futures::{pin_mut, StreamExt};
 use spdx_to_dep5::{
     async_functions::get_record,
+    builder::SPDXBuilder,
+    key_value_parser::KVParser,
     record::{Record, RecordError},
     try_parse_spdx_doc_from_records,
 };
@@ -48,6 +50,22 @@ async fn async_main() -> Result<(), RecordError> {
 }
 fn main() -> Result<(), RecordError> {
     println!("Hello, world!");
-    futures::executor::block_on(async_main())?;
+    // futures::executor::block_on(async_main())?;
+    let file = std::fs::File::open("summary.spdx").unwrap();
+    let mut line_reader = std::io::BufReader::new(file).lines();
+
+    let mut parser = KVParser::new();
+    let mut builder = SPDXBuilder::default();
+    while let Some(result) = line_reader.next() {
+        let line = result.unwrap();
+        if let Some(field) = parser.process_line(&line)?.into_inner() {
+            builder
+                .handle_field(&field)
+                .map_err(|e| RecordError::Message(e.to_string()))?;
+        }
+    }
+    let doc = builder.try_into_result().unwrap();
+
+    println!("stuff: {:?}", doc);
     Ok(())
 }
